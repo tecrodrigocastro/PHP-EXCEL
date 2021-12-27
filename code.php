29e0d7fbc76f31c0727173f4e7ff93c87092e3a8
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2021-12-27 11:04:15 -0300

    final import sucess

--- code.php
+++ code.php
@@ -1,12 +1,13 @@
 <?php
-
+session_start();
+include_once("conection.php");
 require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 
-if(isset($_POST[save_excel_data]))
+if(isset($_POST['save_excel_data']))
 {
     $file_name = $_FILES['import_file']['name'];
     $file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
@@ -17,7 +18,45 @@ if(isset($_POST[save_excel_data]))
     {
         $inputFileNamePath = $_FILES['import_file']['tmp_name'];
         $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileNamePath);
-        $spreadsheet->getActiveSheet()->toArray();
+        $data = $spreadsheet->getActiveSheet()->toArray();
+
+        $count ="0";
+
+        foreach($data as $row)
+        {
+            if($count > 0)
+            {
+
+            
+            $fullname = $row['0'];
+            $email = $row['1'];
+            $phone = $row['2'];
+            $course = $row['3'];
+
+            $studentQuery = "INSERT INTO students (fullname,email,phone,course)
+             VALUES ('$fullname','$email','$phone','$couse')";
+
+             $result = mysqli_query($conn, $studentQuery);
+             $msg = true;
+            }
+            else
+            {
+                $count = "1";
+            }
+        }
+
+        if(isset($msg))
+        {
+        $_SESSION['message'] = "Importado com sucesso";
+        header('Location: index.php');
+        exit(0);
+        }
+        else
+        {
+            $_SESSION['message'] = "Não Importado";
+            header('Location: index.php');
+            exit(0);
+        }
     }
     else
     {
