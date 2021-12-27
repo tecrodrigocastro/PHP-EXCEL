e5a3d49199d7db8ddef6bfd2a97a094245ce95d0
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2021-12-27 10:10:26 -0300

    add and config phpspreadsheet

--- code.php
+++ code.php
@@ -1,7 +1,31 @@
 <?php
 
-if(isset($_POST[save_excel_data])){
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+
+if(isset($_POST[save_excel_data]))
+{
+    $file_name = $_FILES['import_file']['name'];
+    $file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
+
+    $allowed_ext = ['kls','csv','xlsx'];
     
+    if(in_array($file_ext, $allowed_ext))
+    {
+        $inputFileNamePath = $_FILES['import_file']['tmp_name'];
+        $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileNamePath);
+        $spreadsheet->getActiveSheet()->toArray();
+    }
+    else
+    {
+        $_SESSION['message'] = "Arquivo Invalido";
+        header('Location: index.php');
+        exit(0);
+    }
+
 }
 
 ?>
